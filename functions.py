d08d09848fd7dfd4453fc8b4c4c01a3299912125
Odysseas Sofikitis <o.sofikitis@gmail.com>
2023-07-07 01:17:55 +0300

    (WIP) Removes points and triangles outside of photo

--- functions.py
+++ functions.py
@@ -54,3 +54,21 @@ def calculate_normals(verts, faces):
     normals /= norms
 
     return normals
+
+# renders 3d object to 2d photo
+def render_object(shader, focal, eye, lookat, up, bg_color, M, N, H, W, \
+    verts, vert_colors, faces, mat, n, lights, light_amb):
+    # get pixel coordinates of points
+    n2d = projection(verts, H, W, M, N, focal, eye, lookat, up)
+
+    # find the points outside the photo
+    outside_points = np.logical_or.reduce((n2d[0] < 0, n2d[0] > N - 1, n2d[1] < 0, n2d[1] > M - 1))
+
+    # get the indices of those points
+    to_remove = np.where(outside_points)[0]
+
+    # remove the points and corresponding triangles
+    n2d = np.delete(n2d, to_remove, axis=1)
+    faces = faces[:, ~np.isin(faces, to_remove).any(axis=0)]
+
+    return img
